00ab990e9983ae44cb763745bcc62ad6fd17ee21
Victorm200 <victormwaniki2005@gmail.com>
2025-07-28 14:25:39 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle tip visibility when title is clicked
+const titles = document.querySelectorAll(".tip-title");
+
+titles.forEach(title => {
+  title.addEventListener("click", () => {
+    const content = title.nextElementSibling;
+    content.style.display = (content.style.display === "block") ? "none" : "block";
+  });
+});
